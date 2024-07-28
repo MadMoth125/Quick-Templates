78c555a681e6cbeeb0b6ebc0997feb18572112ce
MadMoth125 <madmoth125@gmail.com>
2024-07-28 06:18:55 -0500

    Created TemplateConfigScriptableObject for customizing templates

---
+++ Better Script Templates/Assets/QuickTemplates/TemplateConfigScriptableObject.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using QuickTemplates.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace QuickTemplates
+{
+	[CreateAssetMenu(fileName = "NewTemplateConfig", menuName = "QuickTemplates/Template Config")]
+	public class TemplateConfigScriptableObject : ScriptableObject
+	{
+		public List<TemplateObject> templates;
+
+		[ContextMenu("Load Templates")]
+		public void LoadTemplates()
+		{
+			List<string> paths = TemplateAssetManager.GetTemplatePaths(paths: null);
+			foreach (string path in paths)
+			{
+				if (templates.Any(t => t.GetTemplatePath() == path)) continue;
+
+				TemplateAssetInfo templateInfo = TemplateAssetManager.GetTemplateAssetInfoFromPath(path);
+				TextAsset template = AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+				if (template == null) continue;
+
+				templates.Add(new TemplateObject("Assets/Create/", templateInfo.Name + templateInfo.Extension, template));
+			}
+
+			templates = templates.OrderBy(t => Path.GetDirectoryName(t.GetTemplatePath())?.Replace("\\", "/")).ToList();
+		}
+
+		[ContextMenu("Refresh Menu")]
+		public void RefreshMenu()
+		{
+			TemplateAssetManager.RequestScriptReload();
+		}
+	}
+}
