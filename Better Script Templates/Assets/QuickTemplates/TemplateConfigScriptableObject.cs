3087644873b7e2a8aefd34f90bf4e55c7b72479d
MadMoth125 <madmoth125@gmail.com>
2024-07-29 03:43:43 -0500

    Update TemplateConfigScriptableObject.cs

--- Better Script Templates/Assets/QuickTemplates/TemplateConfigScriptableObject.cs
+++ Better Script Templates/Assets/QuickTemplates/TemplateConfigScriptableObject.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using QuickTemplates.Editor;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace QuickTemplates
 {
-	[CreateAssetMenu(fileName = "NewTemplateConfig", menuName = "QuickTemplates/Template Config")]
+	[CreateAssetMenu(fileName = "NewTemplateConfiguration", menuName = "QuickTemplates/Template Configuration")]
 	public class TemplateConfigScriptableObject : ScriptableObject
 	{
 		public List<TemplateObject> templates;
@@ -16,17 +18,21 @@ namespace QuickTemplates
 		[ContextMenu("Load Templates")]
 		public void LoadTemplates()
 		{
-			List<string> paths = TemplateAssetManager.GetTemplatePaths(paths: null);
-			foreach (string path in paths)
+			foreach (string path in TemplateUtils.GetTemplateAssetPaths())
 			{
 				if (templates.Any(t => t.GetTemplatePath() == path)) continue;
 
-				TemplateAssetInfo templateInfo = TemplateAssetManager.GetTemplateAssetInfoFromPath(path);
-				TextAsset template = AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+				#if UNITY_EDITOR
+				// TemplateAssetInfo templateInfo = TemplateAssetManager.GetTemplateAssetInfoFromPath(path);
+				var template = AssetDatabase.LoadAssetAtPath<TextAsset>(path);
 				if (template == null) continue;
+				#else
+				break;
+				#endif
 
-				string trimmedName = templateInfo.Name.Substring(templateInfo.Name.IndexOf("_", StringComparison.Ordinal) + 1);
-				templates.Add(new TemplateObject("Assets/Create/" + trimmedName, $"New{trimmedName}{templateInfo.Extension}", template));
+				string nonPrefixName = TemplateUtils.GetTemplateName(path, includePrefix: false);
+				string templateExtension = TemplateUtils.GetTemplateExtension(path);
+				templates.Add(new TemplateObject("Assets/Create/Templates/" + nonPrefixName, $"New{nonPrefixName}{templateExtension}", template));
 			}
 		}
 
@@ -39,17 +45,7 @@ namespace QuickTemplates
 		[ContextMenu("Refresh Menu")]
 		public void RefreshMenu()
 		{
-			TemplateAssetManager.RequestScriptReload();
-		}
-
-		public static TemplateConfigScriptableObject GetTemplateConfigs()
-		{
-			List<TemplateConfigScriptableObject> assets = AssetDatabase.FindAssets($"t:{typeof(TemplateConfigScriptableObject)}", null)
-			                                                           .Select(AssetDatabase.GUIDToAssetPath)
-			                                                           .Select(AssetDatabase.LoadAssetAtPath<TemplateConfigScriptableObject>)
-			                                                           .ToList();
-			return assets[0];
-
+			TemplateManager.RequestScriptReload();
 		}
 	}
 }
